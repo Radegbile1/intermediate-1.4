ea8999b2c17b5e0eaf4566970dbf0e9afcab44f5
Radegbile1 <111902181+Radegbile1@users.noreply.github.com>
2023-02-13 23:15:35 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('Stutern Cohort 1.4');
+
+// function that multiplies two numbers
+function multiply (number1, number2) {
+    return number1 * number2;
+}
+
+console.log(multiply(20, 5));
+
+let firstName = 'Funmilayo';
+let lastName = 'Adegbile';
+let stuternTrack = 'frontend';
+let colour = 'pink';
