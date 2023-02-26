c9b3155f1c0ab249bd0dad1df03fb4856014e62a
Radegbile1 <adegbilefunmilayoracheal@gmail.com>
2023-02-26 21:29:02 +0100

    objects

---
+++ 4.objects-assignment/index.js
@@ -0,0 +1,11 @@
+
+//Question 1
+
+function InstagramPost(handleOfAuthors, content, imageLink, numberOfViews, numberOfLikes){
+    this.handleOfAuthors = handleOfAuthors;
+    this.content = content;
+    this.imageLink = imageLink;
+    this.numberOfViews = numberOfViews;
+    this.numberOfLikes = numberOfLikes;
+}
+console.log(handleOfAuthors)
