89b6b1290a831aaccd742e16d240c1e389ddedac
Radegbile1 <adegbilefunmilayoracheal@gmail.com>
2023-02-27 13:41:53 +0100

    objects

--- 4.objects-assignment/index.js
+++ 4.objects-assignment/index.js
@@ -1,5 +1,5 @@
 
-//Question 1
+/*Question 1
 
 function InstagramPost(handleOfAuthors, content, imageLink, numberOfViews, numberOfLikes){
     this.handleOfAuthors = handleOfAuthors;
@@ -8,4 +8,51 @@ function InstagramPost(handleOfAuthors, content, imageLink, numberOfViews, numbe
     this.numberOfViews = numberOfViews;
     this.numberOfLikes = numberOfLikes;
 }
-console.log(handleOfAuthors)
+**/
+
+/*Question2
+
+const post1 = new InstagramPost("@Adegbile1", "business", "EBONY.png", 100, 50)
+const post2 = new InstagramPost("@sheywayne", "love", "EBONY.png", 1000, 20)
+console.log(post1)
+**/
+
+
+/*Question 3A
+
+function createPerson(name, age, location)
+return{
+    name: name,
+    age: age,
+    location: location,
+}
+
+const person1 = createPerson("musa", 19, "Lagos")
+console.log(person1)
+**/
+
+/*Question 3B
+
+function createJambScores(eng, govt, lit, crk)
+return{
+    eng: eng,
+    govt: govt,
+    lit: lit,
+    crk: crk,
+}
+const person1 = createJambScores(70, 85, 82, 94)
+console.log(person1)
+**/
+
+/**
+ Question 4
+ 1 - Using Object.assign()
+    const secondBook = Object.assign({}, firstBook);
+
+ 2 - Using spread syntax i.e, ..
+    const thirdBook = { ...firstBook };
+
+ 3 - Using the method JSON.parse(JSON.stringify())
+    const fourthBook = JSON.parse;
+    
+ */
